23d9b807d26e36c9a79534a7accfa92e279d4e0c
Oredein Oluwatoyin <104014691+teboola@users.noreply.github.com>
2022-06-24 16:08:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let person = {
+    name: "Oluwatoyin",
+    height: "5'8",
+    country: "Nigeria"
+  }
+  
+  const { name, height, country } = person;
+  console.log(name); 
+  console.log(height);
+  console.log(country);
+  
+  alert(name)
+  alert(height)
+  alert(country)
